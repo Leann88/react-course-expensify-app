554b6937a499a4395e79c8a1886d4eb656753fc6
Leann Labelle <leann.88.labelle@gmail.com>
2017-11-27 17:03:25 -0500

    add some test and complete action and reducer code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter'
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss'
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 
+const store = configureStore();
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 900, createdAt: 200 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 200, createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent Bill', amount: 3000, createdAt: 203320 }));
+
+const state = store.getState();
+const visibleExpense = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpense);
+
+const jsx = (
+  <Provider store = {store}>
+    <AppRouter />
+  </Provider>
+)
+ReactDOM.render(jsx, document.getElementById('app'));
