02c3f5c5f3483c48501c0f7039197ce009f47245
Leann Labelle <leann.88.labelle@gmail.com>
2017-11-28 21:28:08 -0500

    style filters and inputs

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -70,11 +70,13 @@ export default class ExpenseForm extends React.Component {
             type="text"
             placeholder="Description"
             autoFocus
+            className="text-input"
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
           <input
             type="text"
+            className="text-input"
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
@@ -89,6 +91,7 @@ export default class ExpenseForm extends React.Component {
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
+            className="textarea"
             value={this.state.note}
             onChange={this.onNoteChange}
           >
